bb5d17336053973523102a31a1fca3972c1efdef
Mohosin Hasan Akash <107780771+Mohosin999@users.noreply.github.com>
2026-02-25 16:10:18 +0600

    Add health check endpoint to app

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ app.get("/", (_req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/health", (_req, res) => {
+  res.status(200).json({message: "Server is Healthy"});
+});
+
 module.exports = app;
